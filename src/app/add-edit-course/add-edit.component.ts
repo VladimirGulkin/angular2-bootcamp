50f6408385df3cb35eeeece27dc9fd470135e68c
Vladimir Gulkin <vladimir_gulkinb@epam.com>
2016-12-21 16:28:16 +0300

    Deleted existing components and added new  pages

---
+++ src/app/add-edit-course/add-edit.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'no-content',
+  template: `
+    <div>
+      <h1>add-edit.component</h1>
+    </div>
+  `
+})
+export class AddEditComponent {
+
+}
