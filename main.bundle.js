17798553ca5fc55c9363d725172a13df1ff17977
VladimirGulkin <vlg.vlad@gmail.com>
2016-12-22 09:54:18 +0000

    283b7b6fbbf6c9070f33aa2fa70578b1acda5e4a commit deployed --skip-ci

---
+++ main.bundle.js
@@ -0,0 +1,611 @@
+var ac_main =
+webpackJsonpac__name_([1],{
+
+/***/ 241:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+// Angular 2
+// rc2 workaround
+var platform_browser_1 = __webpack_require__(71);
+var core_1 = __webpack_require__(0);
+// Environment Providers
+var PROVIDERS = [];
+// Angular debug tools in the dev console
+// https://github.com/angular/angular/blob/86405345b781a9dc2438c0fbe3e9409245647019/TOOLS_JS.md
+var _decorateModuleRef = function identity(value) { return value; };
+if (false) {
+    core_1.enableProdMode();
+    // Production
+    _decorateModuleRef = function (modRef) {
+        platform_browser_1.disableDebugTools();
+        return modRef;
+    };
+    PROVIDERS = PROVIDERS.slice();
+}
+else {
+    _decorateModuleRef = function (modRef) {
+        var appRef = modRef.injector.get(core_1.ApplicationRef);
+        var cmpRef = appRef.components[0];
+        var _ng = window.ng;
+        platform_browser_1.enableDebugTools(cmpRef);
+        window.ng.probe = _ng.probe;
+        window.ng.coreTokens = _ng.coreTokens;
+        return modRef;
+    };
+    // Development
+    PROVIDERS = PROVIDERS.slice();
+}
+exports.decorateModuleRef = _decorateModuleRef;
+exports.ENV_PROVIDERS = PROVIDERS.slice();
+
+
+/***/ },
+
+/***/ 325:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(__webpack_require__(501));
+
+
+/***/ },
+
+/***/ 326:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+var core_1 = __webpack_require__(0);
+var AppState = (function () {
+    function AppState() {
+        this._state = {};
+    }
+    Object.defineProperty(AppState.prototype, "state", {
+        // already return a clone of the current state
+        get: function () {
+            return this._state = this._clone(this._state);
+        },
+        // never allow mutation
+        set: function (value) {
+            throw new Error('do not mutate the `.state` directly');
+        },
+        enumerable: true,
+        configurable: true
+    });
+    AppState.prototype.get = function (prop) {
+        // use our state getter for the clone
+        var state = this.state;
+        return state.hasOwnProperty(prop) ? state[prop] : state;
+    };
+    AppState.prototype.set = function (prop, value) {
+        // internally mutate our state
+        return this._state[prop] = value;
+    };
+    AppState.prototype._clone = function (object) {
+        // simple object clone
+        return JSON.parse(JSON.stringify(object));
+    };
+    return AppState;
+}());
+AppState = __decorate([
+    core_1.Injectable(),
+    __metadata("design:paramtypes", [])
+], AppState);
+exports.AppState = AppState;
+
+
+/***/ },
+
+/***/ 327:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(__webpack_require__(506));
+
+
+/***/ },
+
+/***/ 328:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(__webpack_require__(507));
+
+
+/***/ },
+
+/***/ 329:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(__webpack_require__(508));
+
+
+/***/ },
+
+/***/ 379:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+// App
+__export(__webpack_require__(503));
+
+
+/***/ },
+
+/***/ 501:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+var core_1 = __webpack_require__(0);
+var AddEditComponent = (function () {
+    function AddEditComponent() {
+    }
+    return AddEditComponent;
+}());
+AddEditComponent = __decorate([
+    core_1.Component({
+        selector: 'no-content',
+        template: "\n    <div>\n      <h1>add-edit.component</h1>\n    </div>\n  "
+    }),
+    __metadata("design:paramtypes", [])
+], AddEditComponent);
+exports.AddEditComponent = AddEditComponent;
+
+
+/***/ },
+
+/***/ 502:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+/*
+ * Angular 2 decorators and services
+ */
+var core_1 = __webpack_require__(0);
+var app_service_1 = __webpack_require__(326);
+/*
+ * App Component
+ * Top Level Component
+ */
+var AppComponent = (function () {
+    // angularclassLogo = 'assets/img/angularclass-avatar.png';
+    // name = 'Angular 2 Webpack Starter';
+    // url = 'http://udaff.com';
+    function AppComponent(appState) {
+        this.appState = appState;
+    }
+    AppComponent.prototype.ngOnInit = function () {
+        console.log('Initial App State', this.appState.state);
+    };
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    core_1.Component({
+        selector: 'app',
+        encapsulation: core_1.ViewEncapsulation.None,
+        styles: [
+            __webpack_require__(673)
+        ],
+        template: "\n    <nav>\n      <span>\n        <a [routerLink]=\" ['./'] \">\n          Login\n        </a>\n      </span>\n      |\n      <span>\n        <a [routerLink]=\" ['./courses'] \">\n          Courses\n        </a>\n      </span>\n      |\n      <span>\n        <a [routerLink]=\" ['./courses/new'] \">\n          Add course\n        </a>\n      </span>      \n      | \n      <span>\n        <a [routerLink]=\" ['./courses/:id'] \">\n          Edit course\n        </a>\n      </span>     \n      \n    </nav>   \n\n    <main>\n      <router-outlet></router-outlet>\n    </main>\n\n    <pre class=\"app-state\">this.appState.state = {{ appState.state | json }}</pre>\n\n    <footer>\n      <span>WebPack Angular 2 Starter by <a [href]=\"url\">@AngularClass</a></span>\n      <div>\n        <a [href]=\"url\">\n          <img [src]=\"angularclassLogo\" width=\"25%\">\n        </a>\n      </div>\n    </footer>\n\n  "
+    }),
+    __metadata("design:paramtypes", [typeof (_a = typeof app_service_1.AppState !== "undefined" && app_service_1.AppState) === "function" && _a || Object])
+], AppComponent);
+exports.AppComponent = AppComponent;
+var _a;
+/*
+ * Please review the https://github.com/AngularClass/angular2-examples/ repo for
+ * more angular app examples that you may copy/paste
+ * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
+ * For help or questions please contact us at @AngularClass on twitter
+ * or our chat on Slack at https://AngularClass.com/slack-join
+ */
+
+
+/***/ },
+
+/***/ 503:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+var core_1 = __webpack_require__(0);
+var platform_browser_1 = __webpack_require__(71);
+var forms_1 = __webpack_require__(238);
+var http_1 = __webpack_require__(239);
+var router_1 = __webpack_require__(240);
+var hmr_1 = __webpack_require__(103);
+/*
+ * Platform and Environment providers/directives/pipes
+ */
+var environment_1 = __webpack_require__(241);
+var app_routes_1 = __webpack_require__(505);
+// App is our top level component
+var app_component_1 = __webpack_require__(502);
+var app_resolver_1 = __webpack_require__(504);
+var app_service_1 = __webpack_require__(326);
+var login_1 = __webpack_require__(328);
+var courses_1 = __webpack_require__(327);
+var add_edit_course_1 = __webpack_require__(325);
+var no_content_1 = __webpack_require__(329);
+// Application wide providers
+var APP_PROVIDERS = app_resolver_1.APP_RESOLVER_PROVIDERS.concat([
+    app_service_1.AppState
+]);
+/**
+ * `AppModule` is the main entry point into Angular2's bootstraping process
+ */
+var AppModule = (function () {
+    function AppModule(appRef, appState) {
+        this.appRef = appRef;
+        this.appState = appState;
+    }
+    AppModule.prototype.hmrOnInit = function (store) {
+        if (!store || !store.state)
+            return;
+        console.log('HMR store', JSON.stringify(store, null, 2));
+        // set state
+        this.appState._state = store.state;
+        // set input values
+        if ('restoreInputValues' in store) {
+            var restoreInputValues = store.restoreInputValues;
+            setTimeout(restoreInputValues);
+        }
+        this.appRef.tick();
+        delete store.state;
+        delete store.restoreInputValues;
+    };
+    AppModule.prototype.hmrOnDestroy = function (store) {
+        var cmpLocation = this.appRef.components.map(function (cmp) { return cmp.location.nativeElement; });
+        // save state
+        var state = this.appState._state;
+        store.state = state;
+        // recreate root elements
+        store.disposeOldHosts = hmr_1.createNewHosts(cmpLocation);
+        // save input values
+        store.restoreInputValues = hmr_1.createInputTransfer();
+        // remove styles
+        hmr_1.removeNgStyles();
+    };
+    AppModule.prototype.hmrAfterDestroy = function (store) {
+        // display new elements
+        store.disposeOldHosts();
+        delete store.disposeOldHosts;
+    };
+    return AppModule;
+}());
+AppModule = __decorate([
+    core_1.NgModule({
+        bootstrap: [app_component_1.AppComponent],
+        declarations: [
+            app_component_1.AppComponent,
+            login_1.LoginComponent,
+            courses_1.CoursesComponent,
+            add_edit_course_1.AddEditComponent,
+            no_content_1.NoContentComponent,
+        ],
+        imports: [
+            platform_browser_1.BrowserModule,
+            forms_1.FormsModule,
+            http_1.HttpModule,
+            router_1.RouterModule.forRoot(app_routes_1.ROUTES, { useHash: true, preloadingStrategy: router_1.PreloadAllModules })
+        ],
+        providers: [
+            environment_1.ENV_PROVIDERS,
+            APP_PROVIDERS
+        ]
+    }),
+    __metadata("design:paramtypes", [typeof (_a = typeof core_1.ApplicationRef !== "undefined" && core_1.ApplicationRef) === "function" && _a || Object, typeof (_b = typeof app_service_1.AppState !== "undefined" && app_service_1.AppState) === "function" && _b || Object])
+], AppModule);
+exports.AppModule = AppModule;
+var _a, _b;
+
+
+/***/ },
+
+/***/ 504:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+var core_1 = __webpack_require__(0);
+var Observable_1 = __webpack_require__(10);
+__webpack_require__(659);
+var DataResolver = (function () {
+    function DataResolver() {
+    }
+    DataResolver.prototype.resolve = function (route, state) {
+        return Observable_1.Observable.of({ res: 'I am data' });
+    };
+    return DataResolver;
+}());
+DataResolver = __decorate([
+    core_1.Injectable(),
+    __metadata("design:paramtypes", [])
+], DataResolver);
+exports.DataResolver = DataResolver;
+// an array of services to resolve routes with data
+exports.APP_RESOLVER_PROVIDERS = [
+    DataResolver
+];
+
+
+/***/ },
+
+/***/ 505:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+var login_1 = __webpack_require__(328);
+var courses_1 = __webpack_require__(327);
+var add_edit_course_1 = __webpack_require__(325);
+var no_content_1 = __webpack_require__(329);
+exports.ROUTES = [
+    { path: '', component: login_1.LoginComponent },
+    { path: 'login', component: login_1.LoginComponent },
+    { path: 'courses', component: courses_1.CoursesComponent },
+    { path: 'courses/new', component: add_edit_course_1.AddEditComponent },
+    { path: 'courses/:id', component: add_edit_course_1.AddEditComponent },
+    { path: '**', component: no_content_1.NoContentComponent },
+];
+
+
+/***/ },
+
+/***/ 506:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+var core_1 = __webpack_require__(0);
+var CoursesComponent = (function () {
+    function CoursesComponent() {
+    }
+    return CoursesComponent;
+}());
+CoursesComponent = __decorate([
+    core_1.Component({
+        selector: 'no-content',
+        template: "\n    <div>\n      <h1>courses.component</h1>\n    </div>\n  "
+    }),
+    __metadata("design:paramtypes", [])
+], CoursesComponent);
+exports.CoursesComponent = CoursesComponent;
+
+
+/***/ },
+
+/***/ 507:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+var core_1 = __webpack_require__(0);
+var LoginComponent = (function () {
+    function LoginComponent() {
+    }
+    return LoginComponent;
+}());
+LoginComponent = __decorate([
+    core_1.Component({
+        selector: 'no-content',
+        template: "\n    <div>\n      <h1>login.component</h1>\n    </div>\n  "
+    }),
+    __metadata("design:paramtypes", [])
+], LoginComponent);
+exports.LoginComponent = LoginComponent;
+
+
+/***/ },
+
+/***/ 508:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+var core_1 = __webpack_require__(0);
+var NoContentComponent = (function () {
+    function NoContentComponent() {
+    }
+    return NoContentComponent;
+}());
+NoContentComponent = __decorate([
+    core_1.Component({
+        selector: 'no-content',
+        template: "\n    <div>\n      <h1>404: page missing</h1>\n    </div>\n  "
+    }),
+    __metadata("design:paramtypes", [])
+], NoContentComponent);
+exports.NoContentComponent = NoContentComponent;
+
+
+/***/ },
+
+/***/ 652:
+/***/ function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(653)();
+// imports
+
+
+// module
+exports.push([module.i, "html, body{\n  height: 100%;\n  font-family: Arial, Helvetica, sans-serif\n}\n\nspan.active {\n  background-color: gray;\n}\n", ""]);
+
+// exports
+
+
+/***/ },
+
+/***/ 653:
+/***/ function(module, exports) {
+
+/*
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author Tobias Koppers @sokra
+*/
+// css base code, injected by the css-loader
+module.exports = function() {
+	var list = [];
+
+	// return the list of modules as css string
+	list.toString = function toString() {
+		var result = [];
+		for(var i = 0; i < this.length; i++) {
+			var item = this[i];
+			if(item[2]) {
+				result.push("@media " + item[2] + "{" + item[1] + "}");
+			} else {
+				result.push(item[1]);
+			}
+		}
+		return result.join("");
+	};
+
+	// import a list of modules into the list
+	list.i = function(modules, mediaQuery) {
+		if(typeof modules === "string")
+			modules = [[null, modules, ""]];
+		var alreadyImportedModules = {};
+		for(var i = 0; i < this.length; i++) {
+			var id = this[i][0];
+			if(typeof id === "number")
+				alreadyImportedModules[id] = true;
+		}
+		for(i = 0; i < modules.length; i++) {
+			var item = modules[i];
+			// skip already imported module
+			// this implementation is not 100% perfect for weird media query combinations
+			//  when a module is imported multiple times with different media queries.
+			//  I hope this will never occur (Hey this way we have smaller bundles)
+			if(typeof item[0] !== "number" || !alreadyImportedModules[item[0]]) {
+				if(mediaQuery && !item[2]) {
+					item[2] = mediaQuery;
+				} else if(mediaQuery) {
+					item[2] = "(" + item[2] + ") and (" + mediaQuery + ")";
+				}
+				list.push(item);
+			}
+		}
+	};
+	return list;
+};
+
+
+/***/ },
+
+/***/ 659:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+var Observable_1 = __webpack_require__(10);
+var of_1 = __webpack_require__(70);
+Observable_1.Observable.of = of_1.of;
+//# sourceMappingURL=of.js.map
+
+/***/ },
+
+/***/ 673:
+/***/ function(module, exports, __webpack_require__) {
+
+
+        var result = __webpack_require__(652);
+
+        if (typeof result === "string") {
+            module.exports = result;
+        } else {
+            module.exports = result.toString();
+        }
+    
+
+/***/ },
+
+/***/ 674:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+/*
+ * Angular bootstraping
+ */
+var platform_browser_dynamic_1 = __webpack_require__(147);
+var environment_1 = __webpack_require__(241);
+var hmr_1 = __webpack_require__(103);
+/*
+ * App Module
+ * our top level module that holds all of our components
+ */
+var app_1 = __webpack_require__(379);
+/*
+ * Bootstrap our Angular app with a top level NgModule
+ */
+function main() {
+    return platform_browser_dynamic_1.platformBrowserDynamic()
+        .bootstrapModule(app_1.AppModule).then(function(MODULE_REF) {
+  if (false) {
+    module["hot"]["accept"]();
+    
+    if (MODULE_REF.instance["hmrOnInit"]) {
+      module["hot"]["data"] && MODULE_REF.instance["hmrOnInit"](module["hot"]["data"]);
+    }
+    if (MODULE_REF.instance["hmrOnStatus"]) {
+      module["hot"]["apply"](function(status) {
+        MODULE_REF.instance["hmrOnStatus"](status);
+      });
+    }
+    if (MODULE_REF.instance["hmrOnCheck"]) {
+      module["hot"]["check"](function(err, outdatedModules) {
+        MODULE_REF.instance["hmrOnCheck"](err, outdatedModules);
+      });
+    }
+    if (MODULE_REF.instance["hmrOnDecline"]) {
+      module["hot"]["decline"](function(dependencies) {
+        MODULE_REF.instance["hmrOnDecline"](dependencies);
+      });
+    }
+    module["hot"]["dispose"](function(store) {
+      MODULE_REF.instance["hmrOnDestroy"] && MODULE_REF.instance["hmrOnDestroy"](store);
+      MODULE_REF.destroy();
+      MODULE_REF.instance["hmrAfterDestroy"] && MODULE_REF.instance["hmrAfterDestroy"](store);
+    });
+  }
+  return MODULE_REF;
+})
+        .then(environment_1.decorateModuleRef)
+        .catch(function (err) { return console.error(err); });
+}
+exports.main = main;
+// needed for hmr
+// in prod this is replace for document ready
+hmr_1.bootloader(main);
+
+
+/***/ }
+
+},[674]);
+//# sourceMappingURL=main.map
